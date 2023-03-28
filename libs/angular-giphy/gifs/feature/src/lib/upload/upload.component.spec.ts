92c269562d6b4c14d654f7077ebf36b9caf778eb
Duy Tran <duymon178@gmail.com>
2023-03-28 17:50:50 +0700

    Implement upload design

--- libs/angular-giphy/gifs/feature/src/lib/upload/upload.component.spec.ts
+++ libs/angular-giphy/gifs/feature/src/lib/upload/upload.component.spec.ts
@@ -1,4 +1,6 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DomSanitizer } from '@angular/platform-browser';
 
 import { UploadComponent } from './upload.component';
 
@@ -6,9 +8,16 @@ describe('UploadComponent', () => {
   let component: UploadComponent;
   let fixture: ComponentFixture<UploadComponent>;
 
+  const sanitizerSpy = {
+    sanitize: () => 'safeString',
+    bypassSecurityTrustHtml: () => 'safeString',
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UploadComponent],
+      providers: [{ provide: DomSanitizer, useValue: sanitizerSpy }],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UploadComponent);
--- libs/angular-giphy/gifs/feature/src/lib/upload/upload.component.ts
+++ libs/angular-giphy/gifs/feature/src/lib/upload/upload.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'pi-exchange-upload',
@@ -6,4 +7,35 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./upload.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UploadComponent {}
+export class UploadComponent implements OnDestroy {
+  accept = 'image/gif, image/webp';
+  files: File[] = [];
+  preview = false;
+  images: string[] = [];
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  handleFileInput(event: Event) {
+    this.files = [];
+    const files: FileList | null = (event.target as HTMLInputElement).files;
+
+    if (files) {
+      this.files = Array.from(files);
+      this.images = this.files.map((f) => URL.createObjectURL(f));
+
+      this.preview = true;
+    }
+  }
+
+  getImgSrc(url: string) {
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+
+  ngOnDestroy() {
+    if (this.images.length > 0) {
+      this.images.forEach((url) => {
+        URL.revokeObjectURL(url);
+      });
+    }
+  }
+}
